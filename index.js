3427c7304ae246bb44f8e7c5f31c07de412c8646
MAYANK GAUR <39955543+17mayankgaur@users.noreply.github.com>
2021-07-30 10:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+function dice()
+{
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomImageSource="images/dice"+randomNumber1+".png";
 var image1=document.querySelectorAll("img")[0];
@@ -6,9 +8,6 @@ image1.setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomImageSource2="images/dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
-
-function refresh()//Use to refresh the page onclick
-{location.reload();}
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🎉Player 1 Wins!🎉";
@@ -19,3 +18,5 @@ else if (randomNumber2 > randomNumber1) {
 else {
   document.querySelector("h1").innerHTML = "🏳️ Draw! 🏳️";
 }
+}
+dice();
